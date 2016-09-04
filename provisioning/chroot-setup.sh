435329c5f86ba6d36350e184288b9a42c33dcc30
Jeremy Asher <jeremy@highwarp.com>
2016-09-04 10:14:28 -0700

    pass device name into provisioning script

--- provisioning/chroot-setup.sh
+++ provisioning/chroot-setup.sh
@@ -2,13 +2,15 @@
 
 set -euxo pipefail
 
+source /provisioning/config
+
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 mkinitcpio -p linux
 
 CMDLINE='console=tty0 console=ttyS0,115200n8'
-grub-install --target=i386-pc /dev/xvdf
+grub-install --target=i386-pc "${DEVICE}"
 sed -i 's/\(GRUB_CMDLINE_LINUX_DEFAULT\)=.*/\1=""/' /etc/default/grub
 sed -i "s/\(GRUB_CMDLINE_LINUX\)=.*/\1=\"${CMDLINE}\"/" /etc/default/grub
 echo 'GRUB_TERMINAL=serial' >> /etc/default/grub
